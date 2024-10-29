d46b2ee47705caf5f251e3b99c7c3d8d21b966ac
Arthur Vinicius Batista Sinfrônio <221.14.031@EDU.gruposete.local>
2024-10-28 21:34:56 -0300

    primeiro commit

---
+++ tarefas/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def lista_tarefas(request):
+    tarefas = ["Estudar Django", "Criar um projeto", "Revisar o conteúdo"]
+    return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
